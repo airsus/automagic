1347a6b21789e2001b3bf2e0807bda30ae0eb216
amirreza <amirreza.bahreini@hotmail.com>
2018-07-19 15:08:10 +0200

    upgrade version

--- src/ConstantGlobalValues.m
+++ src/ConstantGlobalValues.m
@@ -18,7 +18,7 @@ classdef ConstantGlobalValues
     % along with this program.  If not, see <http://www.gnu.org/licenses/>.
     properties(Constant)
 
-        version = '1.6.0';
+        version = '1.7.0';
             
         DEFAULT_keyword = 'Default';
                 
